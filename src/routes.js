7a4900d049e88b80f180e1f4062efe481c7130b6
Marcos Travagin <marcostravagin@outlook.com>
2021-06-27 13:53:19 -0300

    route '/'

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import { Router } from 'express'
 import UserController from './app/controllers/UserController'
 const routes = new Router()
 
+routes.get('/', (req, res) => { res.send('This is a test API, to test it acces https://github.com/farelanders/case_hive') })
 routes.post('/v1/users/', UserController.store)
 routes.get('/v1/findbyname/:key/', UserController.findByName)
 routes.get('/v1/findbynickname/:key', UserController.findByNickname)
